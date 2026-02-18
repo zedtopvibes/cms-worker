46e5f3f05129c8531c80b868a928e3bfa1c22c12
zedtopvibes <zedtopvibes@gmail.com>
2026-02-18 03:53:32 +0200

    Update index.js

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,4 +1,3 @@
-
 // ==================== ADMIN MAIN ROUTER ====================
 import { handleAdminLogin, handleAdminLoginPost, handleAdminLogout } from './login.js';
 import { requireAdmin } from '../../middleware/adminAuth.js';
